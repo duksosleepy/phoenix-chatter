adb69dc98798ec9d798e8780e080c35087baa6b8
Song Khoi <songkhoi123@gmail.com>
2025-02-28 00:47:41 +0700

    add js package

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,29 +16,50 @@
 //
 
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
-import "phoenix_html"
+import "mdn-polyfills/Object.assign";
+import "mdn-polyfills/CustomEvent";
+import "mdn-polyfills/String.prototype.startsWith";
+import "mdn-polyfills/Array.from";
+import "mdn-polyfills/Array.prototype.find";
+import "mdn-polyfills/Array.prototype.some";
+import "mdn-polyfills/NodeList.prototype.forEach";
+import "mdn-polyfills/Element.prototype.closest";
+import "mdn-polyfills/Element.prototype.matches";
+import "mdn-polyfills/Node.prototype.remove";
+import "child-replace-with-polyfill";
+import "url-search-params-polyfill";
+import "formdata-polyfill";
+import "classlist-polyfill";
+import "new-event-polyfill";
+import "@webcomponents/template";
+import "shim-keyboard-event-key";
+import "event-submitter-polyfill";
+import "core-js/features/set";
+import "core-js/features/url";
+import "phoenix_html";
 // Establish Phoenix Socket and LiveView configuration.
-import {Socket} from "phoenix"
-import {LiveSocket} from "phoenix_live_view"
-import topbar from "../vendor/topbar"
+import { Socket } from "phoenix";
+import { LiveSocket } from "phoenix_live_view";
+import topbar from "../vendor/topbar";
 
-let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {
-  longPollFallbackMs: 2500,
-  params: {_csrf_token: csrfToken}
-})
+const csrfToken = document
+	.querySelector("meta[name='csrf-token']")
+	.getAttribute("content");
+const liveSocket = new LiveSocket("/live", Socket, {
+	longPollFallbackMs: 2500,
+	params: { _csrf_token: csrfToken },
+});
 
 // Show progress bar on live navigation and form submits
-topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
-window.addEventListener("phx:page-loading-start", _info => topbar.show(300))
-window.addEventListener("phx:page-loading-stop", _info => topbar.hide())
+topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" });
+window.addEventListener("phx:page-loading-start", (_info) => topbar.show(300));
+window.addEventListener("phx:page-loading-stop", (_info) => topbar.hide());
 
 // connect if there are any LiveViews on the page
-liveSocket.connect()
+liveSocket.connect();
 
 // expose liveSocket on window for web console debug logs and latency simulation:
 // >> liveSocket.enableDebug()
 // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
 // >> liveSocket.disableLatencySim()
-window.liveSocket = liveSocket
-
+window.liveSocket = liveSocket;
